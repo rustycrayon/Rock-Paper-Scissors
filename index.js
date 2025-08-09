f8ca9e1666d28a31cd8e3106b22287bd4acc58ad
rustycrayon <214056015+rustycrayon@users.noreply.github.com>
2025-08-09 13:34:56 -0300

    Clean final code Added final game conditionals. Finished code for game.

--- index.js
+++ index.js
@@ -3,72 +3,83 @@
 function getComputerChoice() {
     Math.random()
     if (Math.random() < 0.3) {
-        return "rock"
+        return "Rock"
     }
     else if (Math.random() > 0.6) {
-        return "paper"
+        return "Paper"
     }
     else {
-        return "scissors"
+        return "Scissors"
     }
 }
 
-function getHumanChoice() { // Calling this function prompts the dialogue box 
-    result = prompt("Rock, paper, scissors, go!").toLowerCase();
-    return result
+function getHumanChoice() {
+    answer = prompt("Rock, paper, scissors, go!");
+    finalAnswer = answer.charAt(0).toUpperCase()
+        + answer.slice(1)
+    return finalAnswer 
 }
 
 function playGame() {
-
-    let humanScore = 0;
-    let computerScore = 0;
-
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-
-    function playRound(humanChoice, computerChoice) { // Do I add getHumanChoice() to this function? What do I do to refresh the prompt?
+     
+        let humanScore = 0;
+        let computerScore = 0;
         
-        if (humanChoice === computerChoice) {
-            console.log("Player:", humanChoice, "Computer:", computerChoice)
-            console.log("Tie! Try again")
-            console.log(humanScore, computerScore)
-        }
-        else if (humanChoice === "rock" && computerChoice === "paper") {
-            console.log("Player:", humanChoice, "Computer:", computerChoice)
-            console.log("You lose! Paper beats rock!")
-            console.log(humanScore, ++computerScore)
-             
-        }
-        else if (humanChoice === "paper" && computerChoice === "scissors"){
-            console.log("Player:", humanChoice, "Computer:", computerChoice)
-            console.log("You lose! Scissors beats paper!")
-            console.log(humanScore, ++computerScore)
-              
-        }
-        else if (humanChoice === "scissors" && computerChoice === "rock") {
-            console.log("Player:", humanChoice, "Computer:", computerChoice)
-            console.log("You lose! Rock beats scissors!")
-            console.log(humanScore, ++computerScore)
-              
-        }
-        else if (humanChoice === "rock" && computerChoice === "scissors") {
-            console.log("Player:", humanChoice, "Computer:", computerChoice)
-            console.log("You win! Rock beats scissors!")
-            console.log(++humanScore, computerScore)
-             
-        }
-        else if  (humanChoice === "paper" && computerChoice === "rock") {
-            console.log("Player:", humanChoice, "Computer:", computerChoice)
-            console.log("You win! Paper beats rock!")
-            console.log(++humanScore, computerScore)
-             
+            function playRound(humanChoice, computerChoice) { 
+                
+                if (humanChoice === computerChoice) {
+                    console.log("Player:", humanChoice, "\nComputer:", computerChoice)
+                    console.log("Tie!")
+                    console.log(humanScore, computerScore)
                 }
-        else if (humanChoice === "scissors" && computerChoice === "paper") {
-            console.log("Player:", humanChoice, "Computer:", computerChoice)
-            console.log("You win! Scissors beats paper!")
-            console.log(++humanScore, computerScore)
-                } 
-    }
+                else if (humanChoice === "Rock" && computerChoice === "Paper") {
+                    console.log("Player:", humanChoice, "\nComputer:", computerChoice)
+                    console.log("You lose! Paper beats rock!")
+                    console.log(humanScore, ++computerScore)
+                    
+                }
+                else if (humanChoice === "Paper" && computerChoice === "Scissors"){
+                    console.log("Player:", humanChoice, "\nComputer:", computerChoice)
+                    console.log("You lose! Scissors beats paper!")
+                    console.log(humanScore, ++computerScore)
+                    
+                }
+                else if (humanChoice === "Scissors" && computerChoice === "Rock") {
+                    console.log("Player:", humanChoice, "\nComputer:", computerChoice)
+                    console.log("You lose! Rock beats scissors!")
+                    console.log(humanScore, ++computerScore)
+                    
+                }
+                else if (humanChoice === "Rock" && computerChoice === "Scissors") {
+                    console.log("Player:", humanChoice, "\nComputer:", computerChoice)
+                    console.log("You win! Rock beats scissors!")
+                    console.log(++humanScore, computerScore)
+                    
+                }
+                else if  (humanChoice === "Paper" && computerChoice === "Rock") {
+                    console.log("Player:", humanChoice, "\nComputer:", computerChoice)
+                    console.log("You win! Paper beats rock!")
+                    console.log(++humanScore, computerScore)
+                    
+                        }
+                else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+                    console.log("Player:", humanChoice, "\nComputer:", computerChoice)
+                    console.log("You win! Scissors beats paper!")
+                    console.log(++humanScore, computerScore)
+                        } 
+            }
 
-    return playRound(humanSelection, computerSelection);
+        for (let i = 1; i < 6; i++) {
+            playRound(getHumanChoice(), getComputerChoice());
+        }
+          
+    if (humanScore > computerScore) {
+            console.log("You won the game! Congratulations!")
+        }
+        else if (humanScore < computerScore) {
+            console.log("You lost the game! Try again!")
+        }
+        else {
+            console.log("You tied! Try again!")
+        } 
 }
